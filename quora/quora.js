13a263fe717b9493da6a85776cee83d61250473a
imacemarx <imacemarx@gmail.com>
2023-06-02 20:48:17 +0800

    feat: add chinese name and description

--- quora/quora.js
+++ quora/quora.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name     (2023)Quora: remove sponsored posts
+// @name:zh (2023)Quora:移除列表带sponsored标识的广告
 // @namespace    acemarx
 // @version      1.0.2
 // @description  Remove sponsored posts that are shown as relevant answers even if they're not.
+// @description:zh 自动移除列表里面的广告帖子（带sponsored标识的）
 // @author       acemarx
 // @grant    none
-// @exclude https://*.quora.com/robots.txt?upapi=true
-// @exclude http://*.quora.com/robots.txt?upapi=true
 // @match        https://*.quora.com/*
 // @match        http://*.quora.com/*
 // @license MIT
