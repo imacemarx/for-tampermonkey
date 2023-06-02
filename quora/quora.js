9176b18108c749bef4cb3eeadda9910a795ee41c
imacemarx <imacemarx@gmail.com>
2023-06-02 20:38:24 +0800

    feat: add exclude url, fix repeatedly loaded

--- quora/quora.js
+++ quora/quora.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name     (2023)Quora: remove sponsored posts
 // @namespace    acemarx
-// @version      1.0.1
+// @version      1.0.2
 // @description  Remove sponsored posts that are shown as relevant answers even if they're not.
 // @author       acemarx
 // @grant    none
+// @exclude https://*.quora.com/robots.txt?upapi=true
+// @exclude http://*.quora.com/robots.txt?upapi=true
 // @match        https://*.quora.com/*
 // @match        http://*.quora.com/*
 // @license MIT
